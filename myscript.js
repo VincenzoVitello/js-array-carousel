ce3b1f62673f3f62d90076ee9eed69a218441481
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-15 15:51:36 +0100

    index creata

---
+++ myscript.js
@@ -0,0 +1,30 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+// document.getElementById('big_img').innerHTML()
+//let immagineCarosello = `<img src="img/01.jpg">`;
+let immaginiCarosello = '';
+for(let img=0; img<items.length; img++){
+    immaginiCarosello = '<img src="'+ items[img] +'" >';
+    console.log(immaginiCarosello)
+}
